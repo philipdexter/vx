e09b8b83419b34c6416a8cc0af24a9c33b15b144
Philip Dexter <philip.dexter@gmail.com>
2015-06-30 12:44:13 +0300

    Add simple arguments with argp

--- editor.c
+++ editor.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <wchar.h>
 #include <locale.h>
+#include <argp.h>
 
 #include <Python.h>
 
@@ -40,25 +41,71 @@ PyInit_editor(void)
 	return PyModule_Create(&EditorModule);
 }
 
+const char *argp_program_version = "editor 0.1";
+static char doc[] = "editor -- edit files";
+
+static char args_doc[] = "FILE";
+
+static  struct argp_option options[] = {
+	{"verbose", 'v', 0, 0, "Produce verbose output" },
+	{"nopy", 's', 0, 0, "Don't load any python"},
+	{ 0 }
+};
+
+struct arguments
+{
+	char *args[1];
+	int verbose, nopy;
+};
+
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state)
+{
+	struct arguments *arguments = state->input;
+
+	switch(key) {
+		case 'v':
+			arguments->verbose = 1;
+			break;
+		case 's':
+			arguments->nopy = 1;
+			break;
+		case ARGP_KEY_ARG:
+			if (state->arg_num >= 1)
+				argp_usage(state);
+			arguments->args[state->arg_num] = arg;
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
 int main(int argc, char *argv[])
 {
 	signal(SIGINT, finish);
 
-	PyObject *pName;
-	int i;
+	struct arguments arguments;
+	arguments.verbose = 0;
+	arguments.nopy = 0;
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
 	setlocale(LC_ALL, "en_US.UTF-8");
 
 	wchar_t **wargv = calloc(argc, sizeof(wchar_t*));
-	for(i = 0; i < argc; ++i) {
+	for(int i = 0; i < argc; ++i) {
 		wargv[i] = calloc(1, sizeof(wchar_t) * (strlen(argv[i] + 1)));
 		mbstowcs(wargv[i], argv[i], strlen(argv[i]));
 	}
 
-	Py_SetProgramName(wargv[0]);
-	PyImport_AppendInittab("editor", &PyInit_editor);
-	Py_Initialize();
-	PySys_SetArgv(argc, wargv);
+	if(!arguments.nopy) {
+		Py_SetProgramName(wargv[0]);
+		PyImport_AppendInittab("editor", &PyInit_editor);
+		Py_Initialize();
+		PySys_SetArgv(argc, wargv);
+	}
 
 	initscr();
 	nonl();
@@ -67,9 +114,11 @@ int main(int argc, char *argv[])
 
 	getmaxyx(stdscr, row, col);
 
-	pName = PyUnicode_FromString("start");
-	PyImport_Import(pName);
-	Py_DECREF(pName);
+	if(!arguments.nopy) {
+		PyObject *pName = PyUnicode_FromString("start");
+		PyImport_Import(pName);
+		Py_DECREF(pName);
+	}
 
 	Buffer *buffer = new_buffer();
 	attach_file(buffer, "README.md");
@@ -87,7 +136,9 @@ int main(int argc, char *argv[])
 		if (c == 'q') break;
 	}
 
-	Py_Finalize();
+	if(!arguments.nopy)
+		Py_Finalize();
+
 	finish(0);
 }
 
