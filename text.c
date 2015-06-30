c0de8428389c643a72e13b01c284c984ddaa13ff
Philip Dexter <philip.dexter@gmail.com>
2015-06-30 11:50:06 +0300

    Change a malloc to a calloc in text gap resizing

--- text.c
+++ text.c
@@ -20,7 +20,7 @@ void resize_gap(Text *text, size_t new_gap)
 	size_t new_size = text->size + expand_by;
 	text->size = new_size;
 	char *old_buf = text->buf;
-	text->buf = (char*)malloc(new_size);
+	text->buf = (char*)calloc(1, new_size);
 	for (int i = 0; i < text->gap_start; ++i)
 		text->buf[i] = old_buf[i];
 	free(old_buf);
