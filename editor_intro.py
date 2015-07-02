ea0e913901af5ee16afe0b22bb35cc099c787015
Philip Dexter <philip.dexter@gmail.com>
2015-07-02 19:59:11 +0300

    Improve key binding python code

--- editor_intro.py
+++ editor_intro.py
@@ -1,39 +1,54 @@
 import editor
 
-def C(c):
+def _C(c):
     return chr(0x1f & ord(c))
 
-def M(c):
+def _M(c):
     return chr(0x80 | ord(c))
 
-class keybinding:
+def _tobinding(s):
+    class donothing:
+        def __init__(self): pass
+        def __add__(self, other): return other
+    binding = donothing()
+    for c,n in zip(s,s[1:]+' '):
+        if c == '-':
+            continue
+        elif c == 'C' and n == '-':
+            c = control()
+        elif c == 'M' and n == '-':
+            c = meta()
+        binding = binding + c
+    return binding
+editor.tobinding = _tobinding
+
+class _keybinding:
     def __init__(self, key):
         self.key = key
 
     def __str__(self):
         return str(self.key)
 
-class control(keybinding):
-    def __init__(self):
+class _keymodifier:
+    def __init__(self, mod):
+        self.mod = mod
         pass
 
     def __add__(self, other):
         if type(other) is str:
-            other = keybinding(other)
-        other.key = C(other.key)
+            other = _keybinding(other)
+        other.key = self.mod(other.key)
         return other
 
-class meta(keybinding):
-    def __init__(self):
-        pass
-
-    def __add__(self, other):
+    def __radd__(self, other):
         if type(other) is str:
-            other = keybinding(other)
-        other.key = M(other.key)
+            other = _keybinding(other)
+        other.key = self.mod(other.key)
         return other
 
-def bind(key, command=None):
+def _bind(key, command=None):
+    if type(key) is str:
+        key = tobinding(key)
     if command is None:
         def g(func):
             editor.keymap[str(key)] = func
@@ -44,6 +59,24 @@ def bind(key, command=None):
     else:
         editor.keymap[str(key)] = command
 
-editor.bind = bind
-editor.ctrl = control()
-editor.alt = meta()
+editor.bind = _bind
+editor.ctrl = _keymodifier(_C)
+editor.alt = _keymodifier(_M)
+
+def _repeat(c, times=4):
+    for _ in range(times):
+        c()
+
+editor.repeat = _repeat
+
+class _keys:
+    def __init__(self):
+        for i in range(26):
+            char = chr(ord('a') + i)
+            setattr(_keys, char, char)
+        for i in range(26):
+            char = chr(ord('A') + i)
+            setattr(_keys, char, char)
+        _keys.le = '<'
+        _keys.ge = '>'
+editor.keys = _keys()
--- start.py
+++ start.py
@@ -1,18 +1,27 @@
 import editor
+from editor import bind, alt, ctrl, keys
+
+from functools import partial
 
 editor.status_line = 'status line-------'
 
-@editor.bind(editor.alt + 'd')
-def down_more():
-    editor.move_down()
-    editor.move_down()
-    editor.move_down()
-    editor.move_down()
+bind(ctrl + keys.l, editor.move_down)
 
-editor.bind(editor.ctrl + 'N', editor.move_down)
-editor.bind(editor.ctrl + 'P', editor.move_up)
-editor.bind(editor.ctrl + 'B', editor.move_left)
-editor.bind(editor.ctrl + 'F', editor.move_right)
+bind(ctrl + keys.v, partial(editor.repeat, editor.move_down))
+bind(alt + keys.v, partial(editor.repeat, editor.move_up))
 
-editor.bind(editor.ctrl + 'A', editor.move_bol)
-editor.bind(editor.ctrl + 'E', editor.move_eol)
+bind(ctrl + keys.n, editor.move_down)
+bind(ctrl + keys.p, editor.move_up)
+bind(ctrl + keys.b, editor.move_left)
+bind(ctrl + keys.f, editor.move_right)
+
+bind(ctrl + keys.a, editor.move_bol)
+bind(ctrl + keys.e, editor.move_eol)
+
+bind(ctrl + keys.s, editor.save)
+
+bind(alt + keys.le, editor.move_beg)
+bind(alt + keys.ge, editor.move_end)
+
+bind(alt + keys.s, editor.move_beg)
+
