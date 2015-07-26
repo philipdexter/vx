96d2e3d47cf193046f68fef859244fd31be2ffa9
Philip Dexter <philip.dexter@gmail.com>
2015-07-26 10:14:37 +0300

    Fix vx.expose to not crash when None,None is passed
    Also reformat it using functools.partial

--- utils.py
+++ utils.py
@@ -1,15 +1,14 @@
 import vx
 
+from functools import partial
+
 def _expose(f=None, name=None):
+    if f is None:
+        return partial(_expose, name=name)
     if name is None:
         name = f.__name__.lstrip('_')
     if getattr(vx, name, None) is not None:
         raise AttributeError("Cannot expose duplicate name: '{}'".format(name))
-    if f is None:
-        def g(f):
-            setattr(vx, name, f)
-            return f
-        return g
     setattr(vx, name, f)
     return f
 vx.expose = _expose
