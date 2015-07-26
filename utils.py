4e1ff55e0575e710648867ada8fe421df280fb6a
Philip Dexter <philip.dexter@gmail.com>
2015-07-26 09:23:20 +0300

    Fix vx.expose so it works when a name is passed

--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
 import vx
 
-def _expose(f, name=None):
+def _expose(f=None, name=None):
     if name is None:
         name = f.__name__.lstrip('_')
     if getattr(vx, name, None) is not None:
         raise AttributeError("Cannot expose duplicate name: '{}'".format(name))
+    if f is None:
+        def g(f):
+            setattr(vx, name, f)
+            return f
+        return g
     setattr(vx, name, f)
     return f
 vx.expose = _expose
