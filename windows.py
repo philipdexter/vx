b0f467c2ce95ebbdf1dca71e6d7d7e838438a101
Philip Dexter <philip.dexter@gmail.com>
2015-08-05 11:06:28 +0300

    Disable built-in seek preserving and move logic to keybindings
    This shouldn't be something every keybinding impl has to implement
    so this logic should go somewhere

--- windows.py
+++ windows.py
@@ -10,7 +10,7 @@ def _seek_setting(f):
     @wraps(f)
     def g(*args, **kwargs):
         ret = f(*args, **kwargs)
-        _, _window.focused.last_seeked_column = vx.get_linecol_window(_window.focused)
+        #_, _window.focused.last_seeked_column = vx.get_linecol_window(_window.focused)
         return ret
     return g
 def _seek_preserving(f):
@@ -19,7 +19,7 @@ def _seek_preserving(f):
         ret = f(*args, **kwargs)
         r, c = vx.get_linecol_window(_window.focused)
         if c < _window.focused.last_seeked_column:
-            vx.set_linecol_window(_window.focused, r, _window.focused.last_seeked_column)
+            pass#vx.set_linecol_window(_window.focused, r, _window.focused.last_seeked_column)
             # vx.move_eol_window(_window.focused)
             # _, c = vx.get_linecol_window(_window.focused)
             # if _window.focused.last_seeked_column < c:
