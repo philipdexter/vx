e780f95f50e758f9b7c263cd5f41657f5806316f
Philip Dexter <philip.dexter@gmail.com>
2015-08-09 08:05:04 +0300

    Remove unnecessary content in start.py

--- start.py
+++ start.py
@@ -2,10 +2,6 @@ try:
     import rc
 except ImportError:
     import vx
-    from vx import bind, alt, ctrl, keys, window
-
-    import sys
-    from functools import partial
 
     # which keybinding do we want
     from keybindings import hopscotch
