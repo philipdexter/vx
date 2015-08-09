f622569041c3aae25a243029609d63606b29015f
Philip Dexter <philip.dexter@gmail.com>
2015-08-09 08:59:58 +0300

    Change default keybindings to concat since these are the only ones that work
    At least for now

--- start.py
+++ start.py
@@ -4,6 +4,7 @@ except ImportError:
     import vx
 
     # which keybinding do we want
-    from keybindings import hopscotch
+    from keybindings import concat
+    vx.default_keybindings = concat.load
 
     vx.default_start()
