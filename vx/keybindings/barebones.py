f35536f46795532bf6e9bca37a3a54f1314164d9
Philip Dexter <philip.dexter@gmail.com>
2015-10-14 19:11:33 -0400

    Add backspace and enter to barebones keybindings
    Fix #4

--- vx/keybindings/barebones.py
+++ vx/keybindings/barebones.py
@@ -2,6 +2,8 @@ import vx
 from vx.keybindings import bind, alt, ctrl, keys, keybinding_table
 import vx.movement as move
 
+from functools import partial
+
 def load(window):
     return barebones_keybindings(window)
 
@@ -15,5 +17,8 @@ class barebones_keybindings(keybinding_table):
         self.bind(ctrl + keys.p, move.up)
         self.bind(ctrl + keys.f, move.right)
         self.bind(ctrl + keys.b, move.left)
+
+        self.bind(keys.backspace, self.for_window.backspace)
+        self.bind(keys.enter, partial(self.for_window.add_string, '\n'))
 
         self.bind(ctrl + keys.q, vx.quit)
